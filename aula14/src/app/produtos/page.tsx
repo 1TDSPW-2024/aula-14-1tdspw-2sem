c3ca172e3f7ae2be11c2401eebeba7cacc4eb4be
fmelods <felipemelodesousa4@gmail.com>
2024-09-26 21:45:04 -0300

    Finalizando aula

--- aula14/src/app/produtos/page.tsx
+++ aula14/src/app/produtos/page.tsx
@@ -1,14 +1,24 @@
+"use client";
+
+import { TipoLanche } from "@/types";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
 
-    const lista = [
-        { id: 1, nome: "cachorro-quente", preco: 10.50, desc: "Doguito" },
-        { id: 2, nome: "sanduba-frio", preco: 15.55, desc: "Chapinha" },
-        { id: 3, nome: "misto-cheio", preco: 22.77, desc: "Cheio de Vento" },
-        { id: 4, nome: "miauzito-dalhe", preco: 9.80, desc: "Churrasquinho da tia" },
-        { id: 5, nome: "coxinha-vem-que-tem", preco: 7.50, desc: "Vem que tem nada..." },
-    ];
+    const [lista, setlista] = useState<TipoLanche[]>([]);
+
+    useEffect(() => {
+        
+        const chamadaDaApi = async () =>{
+            const response = await fetch("hhtp://localhost:3000/api/base-route");
+            const dados = await response.json();
+            setlista(dados);
+        };
+
+        chamadaDaApi();
+    }, [])
+    
 
     return (
         <div>
--- aula14/src/app/produtos/produto/[id]/page.tsx
+++ aula14/src/app/produtos/produto/[id]/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Produto({params}:{params:{id:number}}) {
 
-
+  
 
   return (
     <div>
